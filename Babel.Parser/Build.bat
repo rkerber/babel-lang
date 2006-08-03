44a339dec27ca5ca3e7084c634a4ec6b32de7f02
bryanlivingston <bryanlivingston@8919757c-9619-0410-a4b5-35fd234dd263>
2006-08-03 00:09:49 +0000

    Fixed Compile Problems

--- Babel.Parser/Build.bat
+++ Babel.Parser/Build.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Building Babel Grammer...
-Gold\goldbuilder_main Grammer\Babel.grm
-type Grammer\Babel.log
-Gold\testgrammar_main Grammer\Babel.cgt ..\Babeler\bin\debug\TestCases.babel
+echo Building Babel Grammar...
+Gold\goldbuilder_main Grammar\Babel.grm
+type Grammar\Babel.log
+Gold\testgrammar_main Grammar\Babel.cgt ..\Babeler\bin\debug\TestCases.babel
 type ..\Babeler\bin\debug\TestCases.out
