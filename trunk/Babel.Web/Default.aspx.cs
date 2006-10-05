04cd9fd34114382ef018a38aa465cd07ebaa0644
bryanlivingston <bryanlivingston@8919757c-9619-0410-a4b5-35fd234dd263>
2006-10-05 17:39:15 +0000

    Web translator with logging.

--- trunk/Babel.Web/Default.aspx.cs
+++ trunk/Babel.Web/Default.aspx.cs
@@ -2,6 +2,7 @@ using System;
 using System.Data;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
@@ -33,13 +34,20 @@ public partial class _Default : System.Web.UI.Page
         ResultLabel.Text = String.Empty;
 
         foreach(Statement s in parse)
-        {
-            ResultLabel.Text += EnglishEmitter.ToEnglish(s) + "<br>\n";
-        }
+            ResultLabel.Text += EnglishEmitter.ToEnglish(s) + "<br>\r\n";
 
         foreach(Exception exception in parse.ErrorList)
+            ResultLabel.Text += exception.ToString() + "<br>\r\n";
+
+        Log(String.Format("\r\n\r\n\t\t{0} - {1} - {2}\r\n\r\nSource:\r\n{3}\r\n\r\nResult:\r\n{4}", DateTime.Now.ToString(), Request.UserHostAddress, Request.UserAgent, SourceTextBox.Text, ResultLabel.Text.Replace("<br>", String.Empty)));
+    }
+
+    public void Log(string message)
+    {
+        if (message.Length < 10000)
         {
-            ResultLabel.Text += exception.ToString() + "<br>\n";
+            DateTime now = DateTime.Now;
+            File.AppendAllText(Request.PhysicalApplicationPath + "\\logs\\" + now.Year + "-" + now.Month + "-" + now.Day + ".log", message);
         }
     }
 }
